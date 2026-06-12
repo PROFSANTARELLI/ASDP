58713f1c9b7e160ceb433da6615e8f028d91cf1b
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2026-06-12 13:42:18 -0300

    Update pagamentos_factory.py

--- Factory/pagamentos_factory.py
+++ Factory/pagamentos_factory.py
@@ -9,14 +9,21 @@ class Cartao:
 class Boleto:
    def processar(self):
        return "Pagamento realizado via Boleto"
-   class PagamentoFactory:
-   @staticmethod
-   def criar_pagamento(tipo):
-       if tipo == "pix":
-           return Pix()
-       elif tipo == "cartao":
-           return Cartao()
-       elif tipo == "boleto":
-           return Boleto()
-       else:
-           raise ValueError("Método de pagamento inválido")
+      
+
+class PagamentoFactory:
+
+    @staticmethod
+    def criar_pagamento(tipo):
+
+        if tipo == "pix":
+            return Pix()
+
+        elif tipo == "cartao":
+            return Cartao()
+
+        elif tipo == "boleto":
+            return Boleto()
+
+        else:
+            raise ValueError("Método de pagamento inválido")
