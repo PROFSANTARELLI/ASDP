1a69b983b853a19a6db1db0aad5b326116e978bf
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2026-06-22 12:59:01 -0300

    Add SistemaPedidos class with basic methods

---
+++ unidade04/aula03/sistema_pedidos_ruim.py
@@ -0,0 +1,15 @@
+class SistemaPedidos:
+   def salvar_pedido(self):
+       print("Pedido salvo")
+   def enviar_email(self):
+       print("E-mail enviado")
+   def processar_pagamento(self):
+       print("Pagamento processado")
+   def gerar_relatorio(self):
+       print("Relatório gerado")
+
+sistema = SistemaPedidos()
+sistema.salvar_pedido()
+sistema.enviar_email()
+sistema.processar_pagamento()
+sistema.gerar_relatorio()
